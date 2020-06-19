858eae6e1137d34f36bf513ce72b12a5bce4d97b
Peter Hristakos <peter.hristakos@bigpond.com>
2020-06-19 12:59:15 +1000

    order success form

--- src/OrderFailure.js
+++ src/OrderFailure.js
@@ -2,8 +2,45 @@ import React from 'react';
 import "./OrderFailure.css"
 export default function OrderFailure(props) {
     return (
-        <div className="order-failure center" style={{ display: props.navDisplay ? "none" : "block" }}>
-            <h3><i class="fa fa-exclamation-triangle" aria-hidden="true" style={{ fontSize: "100px", color: "yellow" }}></i>Your request was not able to be placed at this time. Please give us a call on 03305 3111 and a member of our team will be able to assist you with your order.</h3>
+
+        <div className="order-failure-wrapper">
+
+
+            {/* 
+            <div class="screenAlert-icon screenAlert-warning scaleWarning">
+                <span class="screenAlert-body pulseWarningIns"></span>
+                <span class="screenAlert-dot pulseWarningIns"></span>
+            </div>
+
+            <div class="screenAlert-icon screenAlert-info scaleInfo">
+                <span class="screenAlert-dot pulseInfoIns"></span>
+                <span class="screenAlert-body pulseInfoIns"></span>
+            </div>
+
+            <div class="screenAlert-icon screenAlert-success animate">
+                <span class="screenAlert-line screenAlert-tip animateSuccessTip"></span>
+                <span class="screenAlert-line screenAlert-long animateSuccessLong"></span>
+                <div class="screenAlert-placeholder"></div>
+                <div class="screenAlert-fix"></div>
+            </div> */}
+
+            <div className="order-failure" style={{ display: props.navDisplay ? "none" : "grid" }}>
+
+                <div class="screenAlert-icon screenAlert-error animate">
+                    <span class="screenAlert-x-mark">
+                        <span class="screenAlert-line screenAlert-left animateXLeft"></span>
+                        <span class="screenAlert-line screenAlert-right animateXRight"></span>
+                    </span>
+                    <div class="screenAlert-placeholder"></div>
+                    <div class="screenAlert-fix"></div>
+                </div>
+                <div className="order-failure-message">
+                    Your request was not able to be processed at this time.
+                    Please call <a href="tel:03 9305 3111">03 9305 3111 </a>
+                    and a member of the team will be able to assist you with your enquiry.
+
+                </div>
+            </div>
         </div>
     )
 }
--- src/OrderSuccess.js
+++ src/OrderSuccess.js
@@ -2,9 +2,21 @@ import React from 'react';
 import "./OrderSuccess.css"
 export default function OrderSuccess(props) {
     return (
-        <div className="order-success" style={{ display: props.navDisplay ? "none" : "block" }}>
+        <div className="order-success-wrapper">
+            <div className="order-success" style={{ display: props.navDisplay ? "none" : "grid" }}>
 
-            <h3><i class="fa fa-check-circle" aria-hidden="true" style={{ fontSize: "100px" }}>Success</i>Your request has been successfully placed. A member of the team will contact you shortly to discuss your request.</h3>
+                <div class="screenAlert-icon screenAlert-success animate">
+                    <span class="screenAlert-line screenAlert-tip animateSuccessTip"></span>
+                    <span class="screenAlert-line screenAlert-long animateSuccessLong"></span>
+                    <div class="screenAlert-placeholder"></div>
+                    <div class="screenAlert-fix"></div>
+                </div>
+                <div className="order-success-message">
+                    Your request has been received. A member of the team contact you shortly to assist you with your enquiry.
+
+                 </div>
+            </div>
         </div>
+
     )
 }
